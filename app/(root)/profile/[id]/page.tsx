e26e60c0fcc742fa9e05818ec7f89a7ed0cea0ff
Thanya <cheveztj@gmail.com>
2025-05-20 13:05:17 -0400

    implemented nav bar layout, functionality of filters not yet implemented. Navigation between home page and profile is set up.

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+
+const Page = async ( { params }: ParamsWithSearch) => {
+    const { id } = await params;
+    return (
+        <div className="wrapper page">
+            <Header subHeader="trickstermusik503@gmail.com" title="TPOE CHEVEZ" userImg="/assets/images/dummy.jpg"/>
+
+            <h1 className="text-2xl font-karla">USER ID: {id}</h1>
+        </div>
+    )
+}
+export default Page
