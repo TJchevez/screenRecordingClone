d19b22941c0a4fc9bd90209fcad413f40b413921
Thanya <cheveztj@gmail.com>
2025-05-20 22:19:00 -0400

    authentication implemented, home page UI Implemented

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import {dummyCards} from "@/constants";
+import VideoCard from "@/components/VideoCard";
 
 const Page = async ( { params }: ParamsWithSearch) => {
     const { id } = await params;
@@ -6,7 +8,11 @@ const Page = async ( { params }: ParamsWithSearch) => {
         <div className="wrapper page">
             <Header subHeader="trickstermusik503@gmail.com" title="TPOE CHEVEZ" userImg="/assets/images/dummy.jpg"/>
 
-            <h1 className="text-2xl font-karla">USER ID: {id}</h1>
+            <section className={"video-grid"}>
+            {dummyCards.map((card) => (
+                <VideoCard key={card.id}{...card}/>
+            ))}
+            </section>
         </div>
     )
 }
