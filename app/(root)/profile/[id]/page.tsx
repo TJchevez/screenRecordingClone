44169962f15eff36c35c1dce1bc2e7f8961aa314
Thanya <cheveztj@gmail.com>
2025-06-11 10:39:43 -0400

    added view count and accurate time for videos

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -3,37 +3,48 @@ import VideoCard from "@/components/VideoCard";
 import {getAllVideosByUser} from "@/lib/actions/video";
 import {redirect} from "next/navigation";
 import EmptyState from "@/components/EmptyState";
+import SharedHeader from "@/components/SharedHeader";
 
-const Page = async ( { params, searchParams }: ParamsWithSearch) => {
+const Page = async ({ params, searchParams }: ParamsWithSearch) => {
     const { id } = await params;
     const { query, filter } = await searchParams;
 
-    const { user, videos } = await getAllVideosByUser(id, query, filter)
-
-    if(!user) redirect('/404');
+    const { user, videos } = await getAllVideosByUser(id, query, filter);
+    if (!user) redirect("/404");
 
     return (
-        <div className="wrapper page">
-            <Header subHeader={user?.email} title={user?.name} userImg={user?.image ?? ''}/>
+        <main className="wrapper page">
+            <SharedHeader
+                subHeader={user?.email}
+                title={user?.name}
+                userImg={user?.image ?? ""}
+            />
 
-            {videos?.length > 0 ?
-                (
-                    <section className="video-grid">
-                        {videos.map(({ video, user }) => (
-                            <VideoCard
-                                key={video.id}
-                                {...video}
-                                thumbnail={video.thumbnailUrl}
-                                userImg={user?.image || ''}
-                                username={user?.name || 'Guest'}
-                            />
-                        ))}
-                    </section>
-                ) : (
-                    <EmptyState icon="/assets/icons/video.svg" title="No Videos Available Yet" description="Video will show up once you upload them"/>
-                )
-            }
-        </div>
-    )
-}
+            {videos?.length > 0 ? (
+                <section className="video-grid">
+                    {videos.map(({ video }) => (
+                        <VideoCard
+                            key={video.id}
+                            id={video.videoId}
+                            title={video.title}
+                            thumbnail={video.thumbnailUrl}
+                            createdAt={video.createdAt}
+                            userImg={user.image ?? ""}
+                            username={user.name ?? "Guest"}
+                            views={video.views}
+                            visibility={video.visibility}
+                            duration={video.duration}
+                        />
+                    ))}
+                </section>
+            ) : (
+                <EmptyState
+                    icon="/assets/icons/video.svg"
+                    title="No Videos Available Yet"
+                    description="Video will show up here once you upload them."
+                />
+            )}
+        </main>
+    );
+};
 export default Page
