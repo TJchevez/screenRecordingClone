46f55200a1bdc03a214cc02ef677cd35cd2c4be1
Thanya <cheveztj@gmail.com>
2026-09-05 13:32:12 -0400

    fix Better Auth trusted origin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,27 @@
-import {betterAuth} from "better-auth"
-import {drizzleAdapter} from "better-auth/adapters/drizzle";
-import {db} from "@/drizzle/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/drizzle/db";
 import { schema } from "@/drizzle/schema";
-import {nextCookies} from "better-auth/next-js";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: 'pg',
+        provider: "pg",
         schema,
     }),
+
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }
+        },
     },
+
     plugins: [nextCookies()],
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
-})
+
+    baseURL: process.env.BETTER_AUTH_URL!,
+
+    trustedOrigins: [
+        "https://screen-recording-clone-gw6f.vercel.app",
+    ],
+});
