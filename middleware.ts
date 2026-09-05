87e1ade52251a6cf352e5ff6c24ba748359e2c28
Thanya <cheveztj@gmail.com>
2026-09-05 12:10:47 -0400

    reduce auth middleware bundle size

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,16 @@
-import {NextRequest, NextResponse} from "next/server";
-import {auth} from "@/lib/auth";
-import {headers} from "next/headers";
-import aj from "@/lib/arcjet";
-import {detectBot, shield} from "arcjet";
-import { createMiddleware } from '@arcjet/next'
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
-export async function middleware(request: NextRequest, response: NextResponse) {
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
+export function middleware(request: NextRequest) {
+    const sessionCookie = getSessionCookie(request);
 
-    if(!session) {
-        return NextResponse.redirect(new URL('/sign-in', request.url))
+    if (!sessionCookie) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
     return NextResponse.next();
 }
 
-const validate = aj
-    .withRule(shield({ mode: 'LIVE' } ))
-    .withRule(detectBot({ mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE', 'GOOGLE_CRAWLER']}))
-
-export default createMiddleware(validate);
-
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+    matcher: ["/upload", "/profile/:path*", "/video/:path*"],
 };
